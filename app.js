b4d7a2e7ddca4323229863bbf1b6598b7be1a96a
Jesse Williamson <j.williamson.sd@gmail.com>
2014-07-16 08:29:07 -0700

    move controller directive. Add soldOut. Add closing html

--- app.js
+++ app.js
@@ -9,6 +9,7 @@
     name: 'Dodecahedron',
     price: 2.95,
     description: 'Some gems have hidden qualities beyond their luster, beyond their shine...Dodeca is one of those gems',
-    canPurchase: false
+    canPurchase: false,
+    soldOut: true
    }
 }());
