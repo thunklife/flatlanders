694d2046745bca087c87438b772fb09869e9d75d
Jesse Williamson <j.williamson.sd@gmail.com>
2014-07-16 09:12:55 -0700

    multiple gems. bootstrap classes

--- app.js
+++ app.js
@@ -2,14 +2,21 @@
    var app = angular.module('store', []);
 
    app.controller('StoreController', function(){
-    this.product = gem;
+    this.products = gems;
    });
 
-   var gem = {
+   var gems = [{
     name: 'Dodecahedron',
     price: 2.95,
     description: 'Some gems have hidden qualities beyond their luster, beyond their shine...Dodeca is one of those gems',
-    canPurchase: false,
-    soldOut: true
-   }
+    canPurchase: true,
+    soldOut: false
+   },
+   {
+    name: 'Pentagonal Gem',
+    price: 5.95,
+    description: 'Some gems have hidden qualities beyond their luster, beyond their shine...Dodeca is one of those gems',
+    canPurchase: true,
+    soldOut: false
+  }];
 }());
