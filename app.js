da58799c8de2669190cb2e634c4d48480d2d5a34
wilhelmson <j.williamson.sd@gmail.com>
2014-07-20 19:33:38 -0700

    products module

--- app.js
+++ app.js
@@ -1,15 +1,8 @@
 /*global angular*/
 (function(){
-  var app = angular.module('store', []);
+  var app = angular.module('store', ['store-products']);
   app.controller('StoreController', function(){
    this.products = gems;
-  });
-
-  app.controller('GalleryController', function(){
-    this.current = 0;
-    this.setCurrent =  function(n){
-      this.current = n || 0;
-    };
   });
   app.controller('ReviewController', function(){
     this.review = {};
@@ -20,50 +13,6 @@
     };
   });
 
-  app.directive('productTitle', function(){
-    return {
-      restrict: 'E',
-      templateUrl: 'product-title.html'
-    };
-  });
-
-  app.directive('productGallery', function(){
-    return {
-      restrict: 'E',
-      templateUrl: 'product-gallery.html',
-      controller: function(){
-        this.current = 0;
-        this.setCurrent = function(n){
-          this.current = n || 0;
-        };
-      },
-      controllerAs: 'gallery'
-    };
-  });
-
-  app.directive('productSpecs', function(){
-    return {
-      restrict: 'A',
-      templateUrl: 'product-specs.html'
-    };
-  });
-
-  app.directive('productPanels', function(){
-    return {
-      restrict: 'E',
-      templateUrl: 'product-panels.html',
-      controller: function(){
-        this.tab = 1;
-        this.selectTab = function(tabNum){
-          this.tab = tabNum;
-        };
-        this.isSelected = function(tabNum){
-          return this.tab === tabNum;
-        };
-      },
-      controllerAs: 'panels'
-    };
-  });
 
   var gems = [{
      name: 'Azurite',
